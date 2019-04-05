cbf8f96ee30515c3ea6355d1db2bf1b7e4904909
Jenna <jmschabdach@gmail.com>
2019-04-05 16:21:05 -0400

    restructured data chapter

--- wordcount.sh
+++ wordcount.sh
@@ -2,4 +2,6 @@
 
 echo "Chapter 1: $(detex ./1/introduction.tex | wc -w)"
 echo "Chapter 2: $(detex ./2/background.tex | wc -w)"
+echo "Data Chapter: $(detex ./data.tex | wc -w)"
+echo "Experiments Chapter: $(detex ./experiments.tex | wc -w)"
 
