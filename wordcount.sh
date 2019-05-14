646e322659d4143481ace85ea95a58dabeb8f316
Jenna <jmschabdach@gmail.com>
2019-05-14 12:12:06 -0400

    working on experiments section

--- wordcount.sh
+++ wordcount.sh
@@ -2,6 +2,6 @@
 
 echo "Chapter 1: $(detex ./1/introduction.tex | wc -w)"
 echo "Chapter 2: $(detex ./2/background.tex | wc -w)"
-echo "Data Chapter: $(detex ./data.tex | wc -w)"
-echo "Experiments Chapter: $(detex ./experiments.tex | wc -w)"
+echo "Chapter 4: Data Chapter: $(detex ./4/data.tex | wc -w)"
+echo "Chapter 5: Experiments Chapter: $(detex ./5/experiments.tex | wc -w)"
 
