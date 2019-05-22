aeeb1d2ddd0031cc2953498aab9ec94288bc98ef
Jenna <jmschabdach@gmail.com>
2019-05-22 13:54:20 -0400

    update includes reorganization and clinical significance chapter

--- wordcount.sh
+++ wordcount.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
-echo "Chapter 1: $(detex ./1/introduction.tex | wc -w)"
-echo "Chapter 2: $(detex ./2/background.tex | wc -w)"
-echo "Chapter 4: Data Chapter: $(detex ./4/data.tex | wc -w)"
-echo "Chapter 5: Experiments Chapter: $(detex ./5/experiments.tex | wc -w)"
+echo "Chapter 1:         Introduction: $(detex ./1/introduction.tex | wc -w)"
+echo "Chapter 2:  Clinical Background: $(detex ./2/clinical_background.tex | wc -w)"
+echo "Chapter 3: Technical Background: $(detex ./3/technical_background.tex | wc -w)"
+echo "Chapter 4:                 Aims: $(detex ./4/aims.tex | wc -w)"
+
+echo "Chapter 5:         Data Chapter: $(detex ./5/data.tex | wc -w)"
+echo "Chapter 6:  Experiments Chapter: $(detex ./6/experiments.tex | wc -w)"
 
