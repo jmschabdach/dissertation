53afcd3f258cdec2aef42d34a15e6122d37aec93
Jenna <jmschabdach@gmail.com>
2019-09-25 18:09:49 -0400

    rearranging complete for now

--- wordcount.sh
+++ wordcount.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 echo "Chapter 1:         Introduction: $(detex ./1/introduction.tex | wc -w)"
-echo "Chapter 2:  Clinical Background: $(detex ./2/clinical_background.tex | wc -w)"
-echo "Chapter 3: Technical Background: $(detex ./3/technical_background.tex | wc -w)"
+echo "Chapter 2: Technical Background: $(detex ./2/technical_background.tex | wc -w)"
+echo "Chapter 3:  Clinical Background: $(detex ./3/clinical_background.tex | wc -w)"
 echo "Chapter 4:              Methods: $(detex ./4/methods.tex | wc -w)"
 
 echo "Chapter 5:                 Data: $(detex ./5/data.tex | wc -w)"
-echo "Chapter 6:          Experiments: $(detex ./6/experiments.tex | wc -w)"
-echo "Chapter 7:              Results: $(detex ./7/results.tex | wc -w)"
-echo "Chapter 8:           Discussion: $(detex ./8/discussion.tex | wc -w)"
+echo "Chapter 6:              Results: $(detex ./6/results.tex | wc -w)"
+echo "Chapter 7:           Discussion: $(detex ./7/discussion.tex | wc -w)"
+echo "Chapter 8:           Conclusion: $(detex ./8/conclusions.tex | wc -w)"
 
