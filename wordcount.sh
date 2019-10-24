bd22016e4fc7e533936f1ec88350a2059cc0cb9c
Jenna <jmschabdach@gmail.com>
2019-10-24 11:13:01 -0400

    added ICA and split 4 into 1 and b

--- wordcount.sh
+++ wordcount.sh
@@ -3,7 +3,8 @@
 echo "Chapter 1:         Introduction: $(detex ./1/introduction.tex | wc -w)"
 echo "Chapter 2: Technical Background: $(detex ./2/technical_background.tex | wc -w)"
 echo "Chapter 3:  Clinical Background: $(detex ./3/clinical_background.tex | wc -w)"
-echo "Chapter 4:              Methods: $(detex ./4/methods.tex | wc -w)"
+echo "Chapter 4:         Moco Methods: $(detex ./4/motion-correction.tex | wc -w)"
+echo "Chapter 4b:          ML Methods: $(detex ./4/methods.tex | wc -w)"
 
 echo "Chapter 5:                 Data: $(detex ./5/data.tex | wc -w)"
 echo "Chapter 6:              Results: $(detex ./6/results.tex | wc -w)"
